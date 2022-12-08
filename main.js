bce435fe20e1022b7fbd9dd3c206996f9aa39a9f
Ravindra898959 <ravindra.064464@gmail.com>
2022-12-08 14:47:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const toTop = document.querySelector(".to-top");
+
+
+
+window.addEventListener("scroll", ()=>{
+    if (window.pageYOffset > 130){
+        toTop.classList.add("active")
+    }else{
+        toTop.classList.remove("active")
+    }
+})
+
+
+
